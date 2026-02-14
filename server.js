177a8ae3f23eda4a5155deac3f561f6981e1d1e6
ashwan331 <ashwanyadav6612@gmail.com>
2026-02-14 09:47:39 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-require("dotenv").config();
-const connectDB = require("./config/db");
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect DB
-connectDB();
-
-// Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/fertilizers", require("./routes/fertilizers"));
 app.use("/api/rentals", require("./routes/rentals"));
 
-// Server
-const PORT = 3000;
-connectDB();
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
